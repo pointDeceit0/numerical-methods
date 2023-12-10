a153d2de365033d33dd329699be1bdc6485fb7e8
pointDeceit0 <fedor.korolev.87@mail.ru>
2023-12-10 17:34:44 +0300

    Added new function

--- helper.py
+++ helper.py
@@ -13,7 +13,7 @@ init_values_1 = [f(x) for x in init_args_1]
 init_values_2 = [f(x) for x in init_args_2]
 
 
-def print_funcan(x, y, func=f) -> None:
+def print_funcan(x: list[float], y: list[float], func=f) -> None:
     """
     Prints analytical and numerical plots 
 
@@ -22,6 +22,31 @@ def print_funcan(x, y, func=f) -> None:
 
     plt.plot(x, y, linewidth=4)   # numerical
     plt.plot(x, func(x)) # analytical
+    plt.legend(["numerical", "analytical"])
+
+    plt.grid(axis="both")
+    # axes
+    ax = plt.subplot()
+    ax.spines['left'].set_position(('data', 0))
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xlabel("x", loc="right")
+    ax.set_ylabel("y", loc="top")
+
+    plt.show()
+
+
+def print_funcan_interpolate(x: list[float], y: list[float], init_args: list[float], init_values: list[float], func=f) -> None:
+    """
+    Prints analytical and numerical plots, also interpollation nodes
+
+    Analytical builds with inition function
+    """
+
+    plt.plot(x, y, linewidth=4)   # numerical
+    plt.plot(x, func(x)) # analytical
+    plt.plot(init_args, init_values, ".")
     plt.legend(["numerical", "analytical"])
 
     plt.grid(axis="both")
