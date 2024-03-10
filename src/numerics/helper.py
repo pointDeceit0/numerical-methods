e9097e12f7f31ca8237dc5e673869e1c58ba5f45
pointDeceit0 <fedor.korolev.87@mail.ru>
2024-03-11 01:42:21 +0300

    Added function which allows to print graphs with ipympl

--- src/numerics/helper.py
+++ src/numerics/helper.py
@@ -40,6 +40,27 @@ def print_funcan(x: list[float], *y: list[list[float]], names=None, func=f) -> N
     plt.show()
 
 
+def print_graphs(x: list[float], *y: list[list[float]], names=None, func=f) -> None:
+    fig, ax = plt.subplots()
+    for g in y:
+        ax.plot(x, g, linewidth=4)
+    ax.plot(x, func(x))
+    if names:
+        names.append("Analytical")
+        ax.legend(names)
+
+    ax.grid(axis="both")
+    # axes
+    ax = plt.subplot()
+    ax.spines['left'].set_position(('data', 0))
+    ax.spines['bottom'].set_position(('data', 0))
+    ax.spines['top'].set_visible(False)
+    ax.spines['right'].set_visible(False)
+    ax.set_xlabel("x", loc="right")
+    ax.set_ylabel("y", loc="top")
+
+
+
 def print_funcan_interpolate(x: list[float], y: list[float], init_args: list[float], init_values: list[float], func=f) -> None:
     """
     Prints analytical and numerical plots, also interpollation nodes
